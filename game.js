d3c58f9de69ecc4a6d75120bfe462fb48191af7d
mateus.goncalves <mateus.goncalves@NBKBHZ0152.ihm.local>
2019-03-08 14:52:13 -0300

    Conseranto hasClass

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+function hasClass(el, className) {
+    return el.classList ? el.classList.contains(className) : new RegExp('\\b'+ className+'\\b').test(el.className);
+}
+
 function btnHTML()
 {
 	document.querySelector("#css_content").classList.remove('fadein-left');
@@ -6,14 +10,10 @@ function btnHTML()
 	document.querySelector("#js_content").classList.remove('fadein-left');
 	document.querySelector("#js_content").classList.add('none');
     
-    if(document.querySelector("#html_content").hasClass('none'))
+    if(hasClass(document.querySelector("#html_content"), 'none'))
     {
     	document.querySelector("#html_content").classList.remove('none');
     	document.querySelector("#html_content").classList.add('fadein-left');
-    }
-    else if(document.querySelector("#html_content").hasClass('none')){
-        document.querySelector("#html_content").classList.remove('none');
-        document.querySelector("#html_content").classList.add('fadein-left');
     }
     else{
         document.querySelector("#html_content").classList.remove('fadein-left');
@@ -29,12 +29,8 @@ function btnCSS()
     document.querySelector("#js_content").classList.remove('fadein-left');
     document.querySelector("#js_content").classList.add('none');
 
-    if(document.querySelector("#css_content").hasClass('none'))
+    if(hasClass(document.querySelector("#css_content"), 'none'))
     {
-        document.querySelector("#css_content").classList.remove('none');
-        document.querySelector("#css_content").classList.add('fadein-left');
-    }
-    else if(document.querySelector("#css_content").hasClass('none')){
         document.querySelector("#css_content").classList.remove('none');
         document.querySelector("#css_content").classList.add('fadein-left');
     }
@@ -46,12 +42,14 @@ function btnCSS()
 
 function btnJS()
 {
-	if(document.querySelector("#js_content").hasClass('none'))
+	document.querySelector("#html_content").classList.remove('fadein-left');
+    document.querySelector("#html_content").classList.add('none');
+
+    document.querySelector("#css_content").classList.remove('fadein-left');
+    document.querySelector("#css_content").classList.add('none');
+	
+	if(hasClass(document.querySelector("#js_content"), 'none'))
     {
-        document.querySelector("#js_content").classList.remove('none');
-        document.querySelector("#js_content").classList.add('fadein-left');
-    }
-    else if(document.querySelector("#js_content").hasClass('none')){
         document.querySelector("#js_content").classList.remove('none');
         document.querySelector("#js_content").classList.add('fadein-left');
     }
@@ -59,9 +57,5 @@ function btnJS()
         document.querySelector("#js_content").classList.remove('fadein-left');
         document.querySelector("#js_content").classList.add('none');
     }
-    document.querySelector("#html_content").classList.remove('fadein-left');
-    document.querySelector("#html_content").classList.add('none');
-
-    document.querySelector("#css_content").classList.remove('fadein-left');
-    document.querySelector("#css_content").classList.add('none');
+    
 }
