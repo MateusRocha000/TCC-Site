6a3dd7b78ecdafce26b8f82c34895ed4ef9579c0
mateus.goncalves <mateus.goncalves@NBKBHZ0152.ihm.local>
2019-03-08 14:42:56 -0300

    Removidos $ no index

--- game.js
+++ game.js
@@ -1,64 +1,67 @@
-$('#btn_html').on("click", function(){
-    $("#css_content").removeClass('fadein-left');
-    $("#css_content").addClass('none');
+function btnHTML()
+{
+	document.querySelector("#css_content").removeClass('fadein-left');
+	document.querySelector("#css_content").addClass('none');
 
-    $("#js_content").removeClass('fadein-left');
-    $("#js_content").addClass('none');
+	document.querySelector("#js_content").removeClass('fadein-left');
+	document.querySelector("#js_content").addClass('none');
     
-    if($("#html_content").hasClass('none'))
+    if(document.querySelector("#html_content").hasClass('none'))
     {
-        $("#html_content").removeClass('none');
-        $("#html_content").addClass('fadein-left');
+    	document.querySelector("#html_content").removeClass('none');
+    	document.querySelector("#html_content").addClass('fadein-left');
     }
-    else if($("#html_content").hasClass('none')){
-        $("#html_content").removeClass('none');
-        $("#html_content").addClass('fadein-left');
+    else if(document.querySelector("#html_content").hasClass('none')){
+        document.querySelector("#html_content").removeClass('none');
+        document.querySelector("#html_content").addClass('fadein-left');
     }
     else{
-        $("#html_content").removeClass('fadein-left');
-        $("#html_content").addClass('none');
+        document.querySelector("#html_content").removeClass('fadein-left');
+        document.querySelector("#html_content").addClass('none');
     }
-});
+}
 
-$('#btn_css').on("click", function(){
-    $("#html_content").removeClass('fadein-left');
-    $("#html_content").addClass('none');
+function btnCSS()
+{
+	document.querySelector("#html_content").removeClass('fadein-left');
+    document.querySelector("#html_content").addClass('none');
 
-    $("#js_content").removeClass('fadein-left');
-    $("#js_content").addClass('none');
+    document.querySelector("#js_content").removeClass('fadein-left');
+    document.querySelector("#js_content").addClass('none');
 
-    if($("#css_content").hasClass('none'))
+    if(document.querySelector("#css_content").hasClass('none'))
     {
-        $("#css_content").removeClass('none');
-        $("#css_content").addClass('fadein-left');
+        document.querySelector("#css_content").removeClass('none');
+        document.querySelector("#css_content").addClass('fadein-left');
     }
-    else if($("#css_content").hasClass('none')){
-        $("#css_content").removeClass('none');
-        $("#css_content").addClass('fadein-left');
+    else if(document.querySelector("#css_content").hasClass('none')){
+        document.querySelector("#css_content").removeClass('none');
+        document.querySelector("#css_content").addClass('fadein-left');
     }
     else{
-        $("#css_content").removeClass('fadein-left');
-        $("#css_content").addClass('none');
+        document.querySelector("#css_content").removeClass('fadein-left');
+        document.querySelector("#css_content").addClass('none');
     }
-});
+}
 
-$('#btn_js').on("click", function(){
-    if($("#js_content").hasClass('none'))
+function btnJS()
+{
+	if(document.querySelector("#js_content").hasClass('none'))
     {
-        $("#js_content").removeClass('none');
-        $("#js_content").addClass('fadein-left');
+        document.querySelector("#js_content").removeClass('none');
+        document.querySelector("#js_content").addClass('fadein-left');
     }
-    else if($("#js_content").hasClass('none')){
-        $("#js_content").removeClass('none');
-        $("#js_content").addClass('fadein-left');
+    else if(document.querySelector("#js_content").hasClass('none')){
+        document.querySelector("#js_content").removeClass('none');
+        document.querySelector("#js_content").addClass('fadein-left');
     }
     else{
-        $("#js_content").removeClass('fadein-left');
-        $("#js_content").addClass('none');
+        document.querySelector("#js_content").removeClass('fadein-left');
+        document.querySelector("#js_content").addClass('none');
     }
-    $("#html_content").removeClass('fadein-left');
-    $("#html_content").addClass('none');
+    document.querySelector("#html_content").removeClass('fadein-left');
+    document.querySelector("#html_content").addClass('none');
 
-    $("#css_content").removeClass('fadein-left');
-    $("#css_content").addClass('none');
-});
+    document.querySelector("#css_content").removeClass('fadein-left');
+    document.querySelector("#css_content").addClass('none');
+}
